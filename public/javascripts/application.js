db5e2a68911f51816891584ec0bd692437754a6b
System Administrator <root@MacBook-de-Alejandro-Toro-Serna.local>
2011-02-21 19:25:10 -0500

    Include the fancybox

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
-	$('.box').fancybox();
+	$('.fancy-contact').fancybox({
+		hideOnOverlayClick: false,
+		overlayColor: '#FFF',
+		showCloseButton: false
+	});
+	
+	$('#contact-form').live('submit', function(e){
+		e.preventDefault();
+		$('#wrapper').block({
+			message: "<img src='/images/ajax-loader.gif' />",
+			overlayCSS: { 
+				backgroundColor: '#FFFFFF', 
+				opacity: 0.6 
+			},
+			css:{
+				border: '0px solid #FFFFFF'
+			}
+		});
+		$(this).ajaxSubmit({
+			success: function(){
+				$('#wrapper').unblock();
+				$.fancybox.close();
+				$.jGrowl("¡Gracias por comunicarse con nosotros!. Pronto nos estaremos comunicando de vuelta.", {life: 10000});
+			}
+		});
+	});
+	/*
+	$('.cancel').live('click', function(){
+		$.fancybox.close();
+	});
+	*/
 });

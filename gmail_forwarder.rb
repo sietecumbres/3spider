9b2a40a5593500bfa2094be624c1ceb4596b4cbd
Juan Espinosa <juan@hlmbusiness.com>
2011-02-28 23:29:04 -0500

    Configuration for mongohq

--- gmail_forwarder.rb
+++ gmail_forwarder.rb
@@ -30,6 +30,16 @@ end
 end
 
 #Configuration
+require 'uri'
+
+if ENV['MONGOHQ_URL']
+    mongo_uri = URI.parse(ENV['MONGOHQ_URL'])
+    ENV['MONGOID_HOST'] = mongo_uri.host
+    ENV['MONGOID_PORT'] = mongo_uri.port.to_s
+    ENV['MONGOID_USERNAME'] = mongo_uri.user
+    ENV['MONGOID_PASSWORD'] = mongo_uri.password
+    ENV['MONGOID_DATABASE'] = mongo_uri.path.gsub("/", "")
+end
 Mongoid.configure do |config|
   name = 'gmail_forwarder'
   host = 'localhost'

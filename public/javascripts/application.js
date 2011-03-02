308af1e49fda432faed51b22cc2e560bb6504e12
Juan Espinosa <juan@hlmbusiness.com>
2011-03-01 21:07:32 -0500

    Fix JS problems

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,35 +1,23 @@
 $(document).ready(function(){
-	$('.fancy-contact').fancybox({
+	$('.box').fancybox({
 		hideOnOverlayClick: false,
 		overlayColor: '#FFF',
 		showCloseButton: false
 	});
 	
 	$('#contact-form').live('submit', function(e){
-		alert('entro');
 		e.preventDefault();
-		$('#wrapper').block({
-			message: "<img src='/images/ajax-loader.gif' />",
-			overlayCSS: { 
-				backgroundColor: '#FFFFFF', 
-				opacity: 0.6 
-			},
-			css:{
-				border: '0px solid #FFFFFF'
-			}
-		});
 		$(this).ajaxSubmit({
 			success: function(){
-				$('#wrapper').unblock();
 				$.fancybox.close();
 				$.jGrowl("¡Gracias por comunicarse con nosotros!. Pronto nos estaremos comunicando de vuelta.", {life: 10000});
 			}
 		});
 	});
-	$('textarea.editor').ckeditor();
-	$('.cancel').live('click', function(){
-		$.fancybox.close();
-	});
+	
+  $('.cancel').live('click', function(){
+    $.fancybox.close();
+  });
 /*	
 	$('.box').fancybox();*/
 	$('textarea.editor').ckeditor();

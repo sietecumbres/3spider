8f5ae26f0745d59068aa2df2154ae4b6c4ab4317
Juan Espinosa <juan@hlmbusiness.com>
2011-03-14 21:29:24 -0500

    Add new mongoid gem and order emails by date

--- lib/models/email.rb
+++ lib/models/email.rb
@@ -4,11 +4,13 @@ class Email
   embedded_in :user, :inverse_of => :emails
   
   field :message_id
-  field :date
+  field :date, :type => Date
   field :subject
   field :subject, :type => Binary
   field :emails_list, :type => Array
   field :from
+  
+  scope :ordered, descending(:date)
   
   def self.find_by_message_id(message_id)
     where(:message_id => message_id).first
--- lib/routes/emails.rb
+++ lib/routes/emails.rb
@@ -3,13 +3,13 @@ get '/emails' do
     adapter = GmailAdapter.new(current_user, 'inbox')
     current_user.store_new_emails(adapter.get_emails)
   
-    emails = current_user.emails.paginate(:per_page => 50, :page => params[:page])
-    @emails = emails.sort{|x, y| y.date <=> x.date}
+    @emails = current_user.emails.ordered.paginate(:per_page => 50, :page => params[:page])
+    #@emails = (emails.sort{|x, y| y.date <=> x.date})
   
     haml :'emails/index', {:layout => :layout}
-  rescue
-    flash[:notice] = 'Usuario o contraseña erroneas'
-    redirect '/login'
+  #rescue
+    #flash[:notice] = 'Usuario o contraseña erroneas'
+    #redirect '/login'
   end
 end
 

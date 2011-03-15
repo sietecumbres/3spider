4bec57ba8c5ba34daa954b2fd7ea009c5ffab744
Juan Espinosa <juan@hlmbusiness.com>
2011-03-14 20:43:32 -0500

    Add email pagination support

--- lib/routes/emails.rb
+++ lib/routes/emails.rb
@@ -1,14 +1,9 @@
 get '/emails' do
-  limit = 20
-  page = (params[:page] || 1).to_i
-  offset = limit * (page -1)
-
   adapter = GmailAdapter.new(current_user, 'inbox')
   current_user.store_new_emails(adapter.get_emails)
   
-  emails = current_user.emails.all :limit => limit, :offset => offset
-  emails = emails.sort{|x, y| y.date <=> x.date}
-  @emails = WillPaginate::Collection.create(page, limit, current_user.emails.count){|p| p.replace emails}
+  emails = current_user.emails.paginate(:per_page => 5, :page => params[:page])
+  @emails = emails.sort{|x, y| y.date <=> x.date}
   
   haml :'emails/index', {:layout => :layout}
 end

079516fff223dbbc528e7ef9ce4f85932df65c22
Juan Espinosa <juan@hlmbusiness.com>
2011-03-14 20:49:32 -0500

    SHow 50 emails per page

--- lib/routes/emails.rb
+++ lib/routes/emails.rb
@@ -2,7 +2,7 @@ get '/emails' do
   adapter = GmailAdapter.new(current_user, 'inbox')
   current_user.store_new_emails(adapter.get_emails)
   
-  emails = current_user.emails.paginate(:per_page => 5, :page => params[:page])
+  emails = current_user.emails.paginate(:per_page => 50, :page => params[:page])
   @emails = emails.sort{|x, y| y.date <=> x.date}
   
   haml :'emails/index', {:layout => :layout}
